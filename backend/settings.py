889ac8b761bf3a64d4557bf5b1c4412a3fc593bd
Abdulwahed Mansour <153433760+abdulwahed-mans@users.noreply.github.com>
2024-11-17 22:16:34 +0100

    Update database settings and modify .gitignore for development environment

--- backend/settings.py
+++ backend/settings.py
@@ -8,6 +8,24 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
+
+
+
+
+# django_project/settings.py
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
+    }
+}
+
+
+
 """
 
 from pathlib import Path
@@ -88,17 +106,14 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 # Database
 
-# django_project/settings.py
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "db",  # set in docker-compose.yml
-        "PORT": 5432,  # default postgres port
+     "default": {
+         "ENGINE": "django.db.backends.sqlite3",
+         "NAME": BASE_DIR / "db.sqlite3",
     }
-}
+ }
 
 
 
